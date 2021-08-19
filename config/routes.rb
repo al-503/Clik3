357926bcd01fa7395b665a4a20472fc2d1806889
Axel Ridray <axel.ridray@sfr.fr>
2021-08-19 16:58:31 +0200

    first restaurant index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "registrations"}
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [ :show, :index] do 
+  resources :restaurants do 
     resources :dishes, only: [ :index ]
   end
 
