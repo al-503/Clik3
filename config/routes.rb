e05f62bf3accbb45a93bc7b77bd71cef370be261
Axel Ridray <axel.ridray@sfr.fr>
2021-08-24 19:51:34 +0200

    dish show routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants do 
-    resources :dishes, only: [ :index ]
+    resources :dishes, only: [ :index, :show]
   end
 
   resources :offers, only: [ :new, :create, :show ]
